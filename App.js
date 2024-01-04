2626346ba917a8ea100943784aed52a8992c6cda
AriyanAk09 <140309694+AriyanAk09@users.noreply.github.com>
2024-01-04 12:07:27 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+function hamBurger () {
+const hamMenu = document.querySelector('.hamburger');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+});
+}
+
+// clock
+let hrs = document.getElementById('hrs');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+setInterval(()=>{
+  let currentTime = new Date();
+  
+  hrs.innerHTML = (currentTime.getHours()< 10 ? '0' : '') + currentTime.getHours();
+  min.innerHTML = (currentTime.getMinutes()< 10 ? '0' : '') + currentTime.getMinutes();
+  sec.innerHTML = (currentTime.getSeconds()< 10 ? '0' : '') + currentTime.getSeconds();
+}, 1000)
+
+// calendar
